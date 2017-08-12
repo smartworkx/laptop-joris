10632302c7c1a12eb30cba88a53da7aecd64fe83
Joris Wijlens <joris@smartworkx.nl>
2017-08-12 14:19:19 +0200

    Added git ignore file and put smartworkx.sh in this repository

---
+++ smartworkx.sh
@@ -0,0 +1,5 @@
+source <(kubectl completion bash)
+
+alias ts="cd ~/workspaces/smartworkx";
+alias tb="cd ~/workspaces/smartworkx/build-server"
+alias tco="cd ~/workspaces/smartworkx/build-server/concourse"
